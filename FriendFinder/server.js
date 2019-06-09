b4d46f7cf6742838336a58f50ccd9c43672ecd1b
bee25141 <bee25141@gmail.com>
2019-06-09 14:36:36 -0500

    html routing works properly on server.js file, need to correct the module exports on htmlRoutes and apiRoutes files

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 //Declaring the express server
 const app = express();
@@ -7,14 +8,24 @@ const app = express();
 let PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 //Router to direct server to display different web pages or get data from the api
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routing/apiRoutes")(app);
+// require("./routing/htmlRoutes")(app);
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
 
 //Listener to detect server
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
